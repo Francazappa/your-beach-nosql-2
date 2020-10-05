71ce0eb085ccc62ebc633d9bc59060970d5ad70b
Simone <simone.francalancia@gmail.com>
2020-10-05 15:52:35 +0200

    Add files via upload
    Seguirò la struttura del codice del backend di justmeet

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+
+
+console.log('\n' + '----- | YOUR BEACH\'S SERVER | -----' + '\n');
+
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+
+// imported routes
+//const usersRoute = require('./routes/users');
+//const adminRoute = require('./routes/admins');
+//const lidoRoute = require('./routes/lidos');
+
+
+// route middlewares
+//app.use('/api/users', usersRoute);
+//app.use('/api/admins', adminRoute);
+//app.use('/api/lidos', lidoRoute);
+
+
+// db connection ==================================================================
+mongoose.connect(
+    process.env.DB_CONNECTION_STRING,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true},
+    () => console.log('- connected to data base')
+);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// =================================================================================
+
+
+// server port
+const port = process.env.PORT;
+app.listen(port, () => console.log('- listening on port ' + port));
